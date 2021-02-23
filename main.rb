6e20515fb46730356a55189a4535144868c267d1
noraworld <mail@noraworld.com>
2021-02-23 18:43:11 +0900

    Split process into when file added and when file modified

--- main.rb
+++ main.rb
@@ -12,38 +12,51 @@ Dotenv.load
 
 post '/payload' do
   JSON.parse(params['payload'])['commits'].each do |commit|
+    commit['added'].each do |new_file_path|
+      next unless ENV['INCLUDED_DIR']&.split(',')&.include?(File.dirname(new_file_path))
+
+      content, description = retrieve_content_and_description(new_file_path)
+      response_body = publish_to_qiita(content, YAML.safe_load(description), new_file_path, mode: :add)
+      map_filepath_with_qiita_item_id(new_file_path, response_body['id'])
+    end
+
     commit['modified'].each do |new_file_path|
       next unless ENV['INCLUDED_DIR']&.split(',')&.include?(File.dirname(new_file_path))
 
-      connection = Faraday.new('https://api.github.com')
-      connection.headers['Accept'] = 'application/vnd.github.VERSION.raw'
-      response = connection.get("/repos/#{ENV['GITHUB_REPOS']}/contents/#{new_file_path}")
-
-      yaml_description = ''
-      yaml_to_be_trimmed = ''
-      if response.body.lines(chomp: true).first == '---'
-        response.body.each_line.with_index do |line, index|
-          if index.zero?
-            yaml_to_be_trimmed += line
-            next
-          end
-
-          if line =~ /^---/
-            yaml_to_be_trimmed += '---'
-            break
-          end
-
-          yaml_description   += line
-          yaml_to_be_trimmed += line
-        end
-      end
-
-      content = response.body.gsub(/\A#{Regexp.escape(yaml_to_be_trimmed)}/, '').gsub(/\A\n+/, '')
-      publish_to_qiita(content, YAML.safe_load(yaml_description), new_file_path, mode: :add)
+      content, description = retrieve_content_and_description(new_file_path)
+      publish_to_qiita(content, YAML.safe_load(description), new_file_path, mode: :edit)
     end
   end
 
   puts 'ok'
+end
+
+def retrieve_content_and_description(new_file_path)
+  connection = Faraday.new('https://api.github.com')
+  connection.headers['Accept'] = 'application/vnd.github.VERSION.raw'
+  response = connection.get("/repos/#{ENV['GITHUB_REPOS']}/contents/#{new_file_path}")
+
+  yaml_description = ''
+  yaml_to_be_trimmed = ''
+  if response.body.lines(chomp: true).first == '---'
+    response.body.each_line.with_index do |line, index|
+      if index.zero?
+        yaml_to_be_trimmed += line
+        next
+      end
+
+      if line =~ /^---/
+        yaml_to_be_trimmed += '---'
+        break
+      end
+
+      yaml_description   += line
+      yaml_to_be_trimmed += line
+    end
+  end
+
+  content = response.body.gsub(/\A#{Regexp.escape(yaml_to_be_trimmed)}/, '').gsub(/\A\n+/, '')
+  [content, yaml_description]
 end
 
 def publish_to_qiita(content, description, new_file_path, mode: nil)
@@ -81,16 +94,27 @@ def publish_to_qiita(content, description, new_file_path, mode: nil)
                    }.to_json
                  end
 
-  connection = Faraday.new('https://qiita.com')
-  response = connection.post do |request|
-    request.url(request_url)
-    request.headers['Authorization'] = "Bearer #{ENV['QIITA_ACCESS_TOKEN']}"
-    request.headers['Content-Type']  = 'application/json'
-    request.body = request_body
+  # TODO: write more smartly!
+  case mode
+  when :add
+    connection = Faraday.new('https://qiita.com')
+    response = connection.post do |request|
+      request.url(request_url)
+      request.headers['Authorization'] = "Bearer #{ENV['QIITA_ACCESS_TOKEN']}"
+      request.headers['Content-Type']  = 'application/json'
+      request.body = request_body
+    end
+  when :edit
+    connection = Faraday.new('https://qiita.com')
+    response = connection.patch do |request|
+      request.url(request_url)
+      request.headers['Authorization'] = "Bearer #{ENV['QIITA_ACCESS_TOKEN']}"
+      request.headers['Content-Type']  = 'application/json'
+      request.body = request_body
+    end
   end
 
-  puts JSON.parse(response.body)['id']
-  map_filepath_with_qiita_item_id(new_file_path, JSON.parse(response.body)['id']) if mode == :add
+  JSON.parse(response.body)
 end
 
 def map_filepath_with_qiita_item_id(filepath, item_id)
@@ -102,7 +126,7 @@ end
 def qiita_item_id(filepath)
   File.open('mapping.txt', 'r') do |file|
     file.each_line do |line|
-      return line.split(',').last.gsub(' ', '') if line.include?(filepath)
+      return line.split(',').last.gsub(/[\s\r\n]/, '') if line.include?(filepath)
     end
   end
 end
