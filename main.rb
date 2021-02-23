cb1a8a1487ef395ddf2749bd849dfce386ff082e
noraworld <mail@noraworld.com>
2021-02-23 14:12:43 +0900

    Split YAML part and Markdown part

--- main.rb
+++ main.rb
@@ -11,13 +11,32 @@ Dotenv.load
 
 post '/payload' do
   JSON.parse(params['payload'])['commits'].each do |commit|
-    commit['added'].each do |new_file|
+    commit['modified'].each do |new_file|
       connection = Faraday.new('https://api.github.com')
       connection.headers['Accept'] = 'application/vnd.github.VERSION.raw'
       response = connection.get("/repos/#{ENV['GITHUB_REPOS']}/contents/#{new_file}")
 
-      # replace this with process that publishes to Qiita
-      puts response.body
+      yaml_description = ''
+      yaml_to_be_trimmed = ''
+      if response.body.lines(chomp: true).first == '---'
+        response.body.each_line.with_index do |line, index|
+          if index.zero?
+            yaml_to_be_trimmed += line
+            next
+          end
+
+          if line =~ /^---/
+            yaml_to_be_trimmed += '---'
+            break
+          end
+
+          yaml_description   += line
+          yaml_to_be_trimmed += line
+        end
+      end
+
+      content = response.body.gsub(/\A#{Regexp.escape(yaml_to_be_trimmed)}/, '').gsub(/\A\n+/, '')
+      puts content
     end
   end
 
