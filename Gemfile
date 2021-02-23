5eaa1b37dd74c0e9025b41eeb7a1058ba75ac090
noraworld <mail@noraworld.com>
2021-02-23 12:29:46 +0900

    Get all contents of files to be pushed

--- Gemfile
+++ Gemfile
@@ -4,5 +4,7 @@ source 'https://rubygems.org'
 
 ruby '2.7.1'
 
+gem 'dotenv'
+gem 'faraday'
 gem 'sinatra'
 gem 'sinatra-contrib'
--- main.rb
+++ main.rb
@@ -1,10 +1,25 @@
 # frozen_string_literal: true
 
+require 'bundler/setup'
+require 'faraday'
+require 'json'
 require 'sinatra'
 require 'sinatra/reloader'
-require 'json'
+
+Bundler.require
+Dotenv.load
 
 post '/payload' do
-  push = JSON.parse(params[:payload])
-  puts "I got some JSON: #{push.inspect}"
+  JSON.parse(params['payload'])['commits'].each do |commit|
+    commit['added'].each do |new_file|
+      connection = Faraday.new('https://api.github.com')
+      connection.headers['Accept'] = 'application/vnd.github.VERSION.raw'
+      response = connection.get("/repos/#{ENV['GITHUB_REPOS']}/contents/#{new_file}")
+
+      # replace this with process that publishes to Qiita
+      puts response.body
+    end
+  end
+
+  puts 'ok'
 end
